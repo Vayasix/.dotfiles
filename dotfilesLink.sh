59f9c2d79f9b3512dd8d37a2b059d7820bcdd503
HIROKI <w.ima1004@gmail.com>
2016-08-12 14:27:13 +0900

    changed

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,8 +5,8 @@ ln -sf ~/unix-dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/unix-dotfiles/.latexmkrc ~/.latexmkrc
 #ln -sf ~/unix-dotfiles/.vimrc.local.bundles ~/.vimrc.local.bundles
 ln -sf ~/unix-dotfiles/.zshrc ~/.zshrc
-ln -sf ~/unix-dotfiles/.zlogin ~/.zlogin
-ln -sf ~/unix-dotfiles/.zlogout ~/.zlogout
-ln -sf ~/unix-dotfiles/.zpreztorc ~/.zpreztorc
-ln -sf ~/unix-dotfiles/.zprofile ~/.zprofile
-ln -sf ~/unix-dotfiles/.zshenv ~/.zshenv 
+# ln -sf ~/unix-dotfiles/.zlogin ~/.zlogin
+# ln -sf ~/unix-dotfiles/.zlogout ~/.zlogout
+# ln -sf ~/unix-dotfiles/.zpreztorc ~/.zpreztorc
+# ln -sf ~/unix-dotfiles/.zprofile ~/.zprofile
+# ln -sf ~/unix-dotfiles/.zshenv ~/.zshenv 
